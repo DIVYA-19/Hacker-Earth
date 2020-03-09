67f6ab8402e2461ad3deebe327c5624073cd7d60
DIVYA-19 <41481377+DIVYA-19@users.noreply.github.com>
2020-03-09 13:51:47 +0530

    Create Corona Virus.py

---
+++ TPC v2.0 Online Round/Corona Virus.py
@@ -0,0 +1,26 @@
+n = int(input())
+s = list(input())
+
+mem = []
+count = 1
+check = 0
+mem2 = []
+for i in range(n):
+    if s[i] == '1':
+        if check%2 == 0:
+            mem.append(i)
+        else:
+            mem2.append(i)
+        check += 1
+print(mem)
+print(mem2)
+if len(mem) == len(mem2):
+    for i in range(0,len(mem)-1):
+        if mem[i+1] - mem2[i] != 1:
+            count *= (mem[i+1] - mem2[i])
+    if count == 0:
+        print(count+1)
+    else:
+        print(count)
+else:
+    print(-1)
