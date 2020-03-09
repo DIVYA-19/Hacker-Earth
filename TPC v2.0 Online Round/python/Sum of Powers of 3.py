5fa636879d0bcf220161e99edd633ed7b5a5cdd4
DIVYA-19 <41481377+DIVYA-19@users.noreply.github.com>
2020-03-09 19:49:02 +0530

    Update Sum of Powers of 3.py

--- TPC v2.0 Online Round/python/Sum of Powers of 3.py
+++ TPC v2.0 Online Round/python/Sum of Powers of 3.py
@@ -1,3 +1,14 @@
+'''
+this problem can be solved by converting the given n into ternary(base 3)
+100 - 10201 -> (1*3^0 + 0*3^1 + 2*3^2 + 0*3^3 + 1*3^4) 
+-- no of powers of 3 used in this are -> 4 (1+2+1)
+we calculate those bits first (bits)
+and compare with given k value
+-- if bits are greater then k value, there is no way to write given num into k powers of 3
+-- we can write 100 as 3^0 + 3^1 + 3^1 + 3^1 + 3^2 + 3^4 -> 6 bits
+-- -- we can divide some numbers into powers of 3 again, when bits are even if we divide some of the numbers we still get even same
+      happen with odd too -- so we check both bits and k values are even or odd
+'''
 q = int(input())
 
 def ternary(n):
