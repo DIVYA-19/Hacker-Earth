7b4ae0c6c36a85f05dca20f52f3456de44d51b68
DIVYA-19 <41481377+DIVYA-19@users.noreply.github.com>
2020-03-09 14:07:24 +0530

    Update Corona Virus.py

--- TPC v2.0 Online Round/python/Corona Virus.py
+++ TPC v2.0 Online Round/python/Corona Virus.py
@@ -17,9 +17,6 @@ if len(mem) == len(mem2):
     for i in range(0,len(mem)-1):
         if mem[i+1] - mem2[i] != 1:
             count *= (mem[i+1] - mem2[i])
-    if count == 0:
-        print(count+1)
-    else:
-        print(count)
+    print(count)
 else:
     print(-1)
