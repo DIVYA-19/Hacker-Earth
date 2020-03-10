b6148741e13ff0f4ce517ff2081ca528c9d46dd2
DIVYA-19 <41481377+DIVYA-19@users.noreply.github.com>
2020-03-10 20:51:52 +0530

    Update Minimum Table Size.cpp

--- TPC v2.0 Online Round/c++/Minimum Table Size.cpp
+++ TPC v2.0 Online Round/c++/Minimum Table Size.cpp
@@ -20,3 +20,22 @@ int main()
 	}
 	cout << ans;	
 }
+
+void usingBinarySearch(){
+	const long long oo 1e6;
+	set <long long> powers;
+	int n,p;
+	cin >> n>>p;
+	for (long long val= power;p<oo;val*p){
+		powers.insert(val);
+	}
+	long long cost = 0
+	for(int i=1; i<=n;i++){
+		int x;
+		cin >> x;
+		auto it = upper_bound(powers.begin(), powers.end()), x-1);
+		
+		cost += *it;
+	}
+	cout << cost ;
+}
